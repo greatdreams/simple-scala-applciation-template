9bd2ab3b6c0efa11a2ef244bf2a8912a0d59c790
Huawei Wang <871155310@qq.com>
2016-08-30 17:38:55 +0800

    change test report style from xml to html

--- build.sbt
+++ build.sbt
@@ -9,6 +9,9 @@ scalaVersion := "2.11.8"
 val logbackVersion = "1.1.7"
 val groovyVersion = "2.4.7"
 
+val myproject = (project in file(".")).disablePlugins(plugins.JUnitXmlReportPlugin)
+(testOptions in  Test) += Tests.Argument(TestFrameworks.ScalaTest, "-h", "target/test-reports")
+
 libraryDependencies ++= Seq(
   "ch.qos.logback" % "logback-core" % s"${logbackVersion}",
   "ch.qos.logback" % "logback-classic" % s"${logbackVersion}",
@@ -16,5 +19,8 @@ libraryDependencies ++= Seq(
   "org.codehaus.groovy" % "groovy-all" % s"${groovyVersion}",
 
   // Change this to another test framework if you prefer
-  "org.scalatest" %% "scalatest" % "3.0.0" % "test"
+
+  "org.scalatest" %% "scalatest" % "3.0.0" % "test->*" excludeAll(
+    ExclusionRule(organization = "org.junit", name="junit")
+    )
 )
--- src/test/scala/Set1Spec.scala
+++ src/test/scala/Set1Spec.scala
@@ -4,6 +4,33 @@ import org.scalatest.{FlatSpec, GivenWhenThen}
   * Created by greatdreams on 8/30/16.
   */
 class Set1Spec extends FlatSpec with GivenWhenThen {
+  markup {
+    """
+      |Mutable Set
+      |———--
+      |
+      |A set is a collection that contains no duplicate elements.
+      |
+      |To implement a concrete mutable set, you need to provide implementations
+      |of the following methods:
+      |
+      |    def contains(elem: A): Boolean
+      |    def iterator: Iterator[A]
+      |    def += (elem: A): this.type
+      |    def -= (elem: A): this.type
+      |
+      |If you wish that methods like `take`,
+      |`drop`, `filter` return the same kind of set,
+      |you should also override:
+      |
+      |    def empty: This
+      |
+      |It is also good idea to override methods `foreach` and
+      |`size` for efficiency.
+      |
+
+    """.stripMargin
+  }
   "A mutable Set" should "allow an element to be added" in {
     Given("an empty mutable Set")
     var set = Set.empty[String]
@@ -18,5 +45,12 @@ class Set1Spec extends FlatSpec with GivenWhenThen {
     assert(set.contains("clarity"))
 
     info("That's all folks")
+    /*
+    info("info is recorded")
+    markup("markup is *also* recorded")
+    note("notes are sent immediately")
+    alert("alerts are also sent immediately")
+    */
+    markup("This test finished with a **bold** statement!")
   }
 }
